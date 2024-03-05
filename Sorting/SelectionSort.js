6af801b2fcd526c100c21d96969a110c2dafef54
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-05 06:55:42 +0530

    selection sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,21 @@
+// Ques 2: Implement Selection Sort in JavaScript
+// Write a function to sort the given array nums in ascending order.
+
+// Input: nums = [29,10,14,37,14]  ----->>>>>  Output: [10,14,14,29,37]
+
+const selectionSort = (arr) => {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+};
+console.log(selectionSort([29, 10, 14, 37, 14]));
