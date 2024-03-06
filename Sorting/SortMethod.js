eff539046cc8fbd0c07bd49ee1d3d73517ba277f
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-06 06:59:17 +0530

    sort method with object

--- Sorting/SortMethod.js
+++ Sorting/SortMethod.js
@@ -21,3 +21,19 @@ const compareFunction = (a, b) => {
 //if we want in ascending order then do return a-b
 //if we want in descending order then do retun b-a
 console.log(arr.sort(compareFunction));
+
+//*sort method with objects
+const people = [
+  { name: "Piyush", age: 25 },
+  { name: "Suyash", age: 22 },
+  { name: "Raj", age: 30 },
+  { name: "Ishan", age: 27 },
+];
+const compareFunctionForObj = (a, b) => {
+  // 1. <0 => a comes first
+  // 2. 0  => nothing changes
+  // 3. >0 => b comes first
+
+  return a.age - b.age;
+};
+console.log(people.sort(compareFunctionForObj));
