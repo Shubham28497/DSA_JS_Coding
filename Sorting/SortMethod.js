dcb67141a89d31e27ae76a2f213aaf169e94ccae
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-06 06:55:14 +0530

    consequences of sort method with number

--- Sorting/SortMethod.js
+++ Sorting/SortMethod.js
@@ -3,3 +3,9 @@
 const fruits = ["dragon fruit", "apple", "date", "cherry", "banana"];
 
 console.log(fruits.sort());
+
+const nums = [10, 5, 18, 1, 27];
+console.log(nums.sort()); 
+// it should give 1,5,10,18,27
+//but it gives 1,10,18,27,5 bcz first it convert it into string
+//and take first alphabet 
