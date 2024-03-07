a5fa7592ae33faec1d9980a948de815ba233731d
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-07 06:42:14 +0530

    push the code to find the kth largest el in simp way

--- Sorting/kthlargEleInArr.js
+++ Sorting/kthlargEleInArr.js
@@ -2,9 +2,9 @@
 
 //input nums=[3,2,1,5,6,4] k=2;  1 2 3 4 5 6
 //output: 5
-
+//Input: A[] = {1, 2, 6, 4, 5}, K = 3
 const findkthLarg = (nums, k) => {
-  nums.sort((a, b) => a - b);
-  return nums[nums.length - k];
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
 };
-console.log(findkthLarg([3, 2, 1, 5, 6, 4], 2));
+console.log(findkthLarg([1, 2, 6, 4, 5], 3));
