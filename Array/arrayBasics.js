274a0c32dc2f8aac286dcd21746208dad38e02c3
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-12 07:05:04 +0530

    dec of arr and know the length

---
+++ Array/arrayBasics.js
@@ -0,0 +1,10 @@
+// Arrays in Javascript
+
+// Decalaration
+let person = {
+  name: "Piyush",
+  age: 25,
+};
+let arr = ["apple", "banana", "cherry", person];
+
+console.log(arr.length); // know the length of an array
