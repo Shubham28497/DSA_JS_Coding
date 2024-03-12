2fa5d2144b26c678d149a04da0bad830f54f353b
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-12 07:12:03 +0530

    looping in arrays

--- Array/arrayBasics.js
+++ Array/arrayBasics.js
@@ -24,4 +24,17 @@ console.log(arr);
 // Remove From Top of the Array
 arr.shift();
 console.log(arr);
+// # Looping Arrays
 
+// For Loop
+for (let i = 0; i < arr.length; i++) {
+  // console.log(arr[i]);
+}
+
+// While Loop
+let i = 0;
+while (i < arr.length) {
+  // console.log(arr[i]);
+
+  i++;
+}
