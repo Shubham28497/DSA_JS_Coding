0061991d28aaa5736604689af046a1aa0d4028f0
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-14 06:36:23 +0530

    remove dup element with js methods

---
+++ Array/removeDuplicates.js
@@ -0,0 +1,20 @@
+// Ques 3 - Remove Duplicates from Sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove
+// the duplicates in-place such that each unique element appears
+// only once.The relative order of the elements should be kept
+// the same.Then return the number of unique elements in nums.
+
+// Input: [1,1,2]               ----->>>>>  Output: 2, [1,2,_]
+// Input: [0,0,1,1,1,2,2,3,3,4] ----->>>>>  Output: 5, [0,1,2,3,4,_,_,_,_,_]
+//with js methods
+function removeDuplicates(nums){
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==nums[i+1]){
+            nums.splice(i+1,1)
+            i--
+                }
+    }
+                return nums.length;
+
+}
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
