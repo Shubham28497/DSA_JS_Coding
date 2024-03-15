f55c77eabc5a39faaabfc05026acd20fa1443984
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-15 06:31:03 +0530

    kadane algo

---
+++ Array/maxSubArray.js
@@ -0,0 +1,25 @@
+// Ques 4 - Given an integer array nums, find the subarray with the largest sum,
+// and return its sum.
+
+// Input: [-2,1,-3,4,-1,2,1,-5,4]     ----->>>>>    Output: 6,   [4,-1,2,1]
+// Input: [5,4,-1,7,8]                ----->>>>>    Output: 23,  [5,4,-1,7,8]
+
+
+//Kadane's Algo
+function maxSubArray(nums) {
+  let sum = 0;
+  let max = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum > max) {
+      max = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+
+  return max;
+}
+console.log(maxSubArray([5, 4, -1, 7, 8]));
