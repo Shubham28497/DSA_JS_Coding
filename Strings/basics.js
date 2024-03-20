a4fc0c2c364acaa492ebb787294a5b955bbd4dd3
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-20 06:10:56 +0530

    creating string in diff ways

---
+++ Strings/basics.js
@@ -0,0 +1,17 @@
+// Strings in Javascript
+
+// Creating Strings
+const string = new String("Subscribe to RoadsideCoder!");
+
+// using double quotes
+const doubleQuote = "Subscribe to RoadsideCoder!";
+
+// using single quotes
+const singleQuote = "RoadsideCoder!";
+
+// using backticks
+const backticks = `
+Subscribe to RoadsideCoder! 
+Like this Video
+Share this video
+`;
